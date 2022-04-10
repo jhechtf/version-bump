60618f2ef1fcf74523042ad112f39f73db4358b6
Jim Burbridge <jhechtf@gmail.com>
2022-04-10 01:25:49 -0700

    wip: Gets the basic setup for the tests up and going.
    Need to determine how to get and validate the command runs

--- mod.test.ts
+++ mod.test.ts
@@ -1,125 +1,149 @@
-import { emptyDir } from 'deps';
+import { assertEquals, emptyDir } from 'deps';
 
 import { runCommand } from 'src/util.ts';
 import { Commit } from 'src/commit.ts';
 
-const providers = [
-  'github.com',
-  'gitlab.com',
-  'bitbucket.com'
-];
-
 const strategies = [
   'deno',
-  'node'
+  'node',
 ];
+
+type UnsavedCommit = Omit<Commit, 'sha' | 'author'> & {
+  tag?: string;
+};
 
 const GIT_USERNAME = 'Testing';
 const GIT_EMAIL = 'testing@email.com';
 
-type UnsavedCommit =  Omit<Commit, 'sha' | 'author'> & {
-  tag?: string;
-};
-
-const commits: UnsavedCommit[] = [
-  {
-    subject: 'fix: Testing'
-  },
-  {
-    subject: 'feat: Added feature',
-    body: 'this has a body'
-  },
-  {
-    subject: 'feat: New cool feature',
-    body: 'BREAKING testing'
-  }
-];
-
 const basePackageDir = 'packages';
 
-async function writeGitHistory(dir: string, remote: string, user: { username: string; email: string; }, commits: UnsavedCommit[]): Promise<boolean> {
-  console.info(dir, user, commits);
+async function writeGitHistory(
+  dir: string,
+  remote: string,
+  user: { username: string; email: string },
+  commits: UnsavedCommit[],
+): Promise<boolean> {
+  console.group(dir);
 
+  console.info('initializing git in repo ');
   // Step 1: initiate Git
   await runCommand('git', [
-    'init'
+    'init',
   ], dir);
 
+  console.info('Configuring username');
   // Add username + email needed for commits
   await runCommand('git', [
     'config',
     'user.name',
-    user.username
+    user.username,
   ], dir);
 
+  console.info('Configuring email');
   await runCommand('git', [
     'config',
     'user.email',
-    user.email
+    user.email,
   ], dir);
 
+  console.info('Adding origin', remote);
   // Add remote url
   await runCommand('git', [
     'remote',
     'add',
     'origin',
-    remote
+    remote,
   ], dir);
-  
-  // Step 2: Add commits. 
 
-  for(const commit of commits) {
-    console.info('Adding commit ', commit);
+  // Step 2: Add commits.
+
+  for (const commit of commits) {
+    console.info('Writing commit', commit.subject);
     const args = ['commit', '-m', commit.subject];
-    if(commit.body) args.push('-m', commit.body);
-    // Need to allow empty changes.
+    if (commit.body) args.push('-m', commit.body);
+
     args.push('--allow-empty');
 
     await runCommand('git', args, dir);
-  }
 
-  return false;
+    if (commit.tag) await runCommand('git', ['tag', commit.tag], dir);
+  }
+  console.groupEnd();
+
+  return true;
 }
+
+const commitMap: UnsavedCommit[] = [
+  {
+    subject: 'chore: initial commit',
+  },
+  {
+    subject: 'chore(release): 0.1.0',
+    tag: 'v0.1.0',
+  },
+  {
+    subject: 'fix: fixed render bug',
+    body: 'some random body text',
+  },
+  {
+    subject: 'chore(release): 0.1.1',
+    tag: 'v0.1.1',
+  },
+  {
+    subject: 'feat: Adds in new feature',
+    body: 'assorted body text',
+  },
+];
 
 async function setupTestingEnv(): Promise<void> {
   console.log('Setting up mock packages...');
-  for(const provider of  providers) {
-    for(const strategy of strategies) {
-      console.info(`Writing to ${basePackageDir}/${provider}/${strategy}`);
-      await emptyDir(`${basePackageDir}/${provider}/${strategy}`);
-      if(strategy === 'node') {
-        console.info('Writing package.json file...');
-        await Deno.writeTextFile(`${basePackageDir}/${provider}/${strategy}/package.json`, `{"version": "0.1.0"}`);
-      } else if (strategy  === 'deno') {
-        console.info('Writing deps.ts file');
-        await Deno.writeTextFile(`${basePackageDir}/${provider}/${strategy}/deps.ts`, `export const VERSION =  "0.1.0";`)
-      }
+  for (const strategy of strategies) {
+    console.info(`Working for repo with strategy ${strategy}`);
+    const packageUrl = `packages/github.com/${strategy}`;
+    // Makes the empty directory.
+    await emptyDir(packageUrl);
+    await writeGitHistory(packageUrl, 'ssh://github.com:user/some-repo.git', {
+      username: GIT_USERNAME,
+      email: GIT_EMAIL,
+    }, commitMap);
 
-      console.info('Write fake commits now...');
-      await writeGitHistory(`${basePackageDir}/${provider}/${strategy}`, 
-        `ssh://${provider}:user/some-repo.git`,
-      {
-        username: GIT_USERNAME,
-        email: GIT_EMAIL
-      }, commits);
+    if (strategy === 'deno') {
+      await Deno.writeTextFile(
+        `${packageUrl}/deps.ts`,
+        `export const VERSION = "0.1.1"`,
+      );
+    } else {
+      await Deno.writeTextFile(
+        `${packageUrl}/package.json`,
+        `{"version": "0.1.1"}`,
+      );
     }
   }
-  
+}
+
+async function tearDown() {
+  await emptyDir('packages');
+  await Deno.remove('packages', { recursive: true });
 }
 
 Deno.test('CLI Test', async (t) => {
+  // Setup Testing Environment
   await setupTestingEnv();
-  // await t.step({
-  //   name: 'Testing Deno Preset',
-  //   fn: async () => {
-  //     const output = await runCommand('git', ['log'], 'packages/deno');
-  //     console.info(output.stdout);
-  //     const commandRun = await runCommand(
-  //       'deno',
-  //       ['run', '-A', '../../mod.ts'],
-  //       'packages/deno',
-  //     );
-  //     console.log(commandRun);
-  //   },
-  // });
+  await t.step({
+    name: 'Testing Deno Preset',
+    fn: async () => {
+      const output = await runCommand(
+        'git',
+        ['log'],
+        'packages/github.com/deno',
+      );
+      console.info(output);
+    },
+  });
+
+  // Remove the packages if we aren't skipping teardown to do an inspection of
+  // the directories for reasons.
+  if (!Deno.args.includes('--skipTeardown')) {
+    await tearDown();
+  }
 });
