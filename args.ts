64b53f138fd9179779b2fc39a2387326c469eaa1
Jim Burbridge <jhechtf@gmail.com>
2023-07-22 14:48:55 -0700

    chore(cleanup): Removes some remnants from moving DI containers

--- args.ts
+++ args.ts
@@ -1,6 +1,6 @@
 import { type Args, parse } from './deps.ts';
 import { argDefaults } from './defaults.ts';
-import { container as inversifyContainer } from './src/container.ts';
+import { container } from './src/container.ts';
 
 /**
  * @description returns a parsed deno argument
@@ -15,6 +15,6 @@ const parsed = parse(Deno.args, {
   default: argDefaults,
 });
 
-inversifyContainer.bind<Args>('args').toConstantValue(parsed);
+container.bind<Args>('args').toConstantValue(parsed);
 
 export default parsed;
--- src/cwd.ts
+++ src/cwd.ts
@@ -1,5 +1,5 @@
-import { container as inversifyContainer } from './container.ts';
+import { container } from './container.ts';
 
 // How to get this to be a value that we can override if necessary?
 // Currently no use cases exist in my mind to do that, but still worth a thought.
-inversifyContainer.bind('cwd').toConstantValue(Deno.cwd());
+container.bind('cwd').toConstantValue(Deno.cwd());
